511492837ac45ce2e641bbe8acd219e71b8ac213
chanseok <miss3378@naver.com>
2019-03-06 16:18:53 +0900

    Initial Commit

---
+++ test20190306002/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  test20190306002
+//
+//  Created by D7703_21 on 2019. 3. 6..
+//  Copyright © 2019년 D7703_21. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        self.view.backgroundColor = UIColor.blue
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}

6e2288108ff5e273702ab6629cf1bd49c8c155fd
chanseok <miss3378@naver.com>
2019-03-06 16:33:37 +0900

    change backgroundColor blue -> red

--- test20190306002/ViewController.swift
+++ test20190306002/ViewController.swift
@@ -13,7 +13,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        self.view.backgroundColor = UIColor.blue
+        self.view.backgroundColor = UIColor.red
     }
 
     override func didReceiveMemoryWarning() {
